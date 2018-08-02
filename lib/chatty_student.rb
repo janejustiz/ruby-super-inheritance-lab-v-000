48b097742ecd59278687363ed10a715be62548df
Jane Justiz <janejustiz@gmail.com>
2018-08-02 07:26:25 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,7 +1,9 @@
+
+
 class ChattyStudent < Student
 
   def hello
-    "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
+    puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...\n"
   end
 
   def raise_hand
